b6f988e8d0468838eb4fb310780e4eb8c35b6d99
Krzych <krzych.pierzchala@gmail.com>
2018-01-04 07:01:49 +0100

    Fixed comments reducer function

--- comments.js
+++ comments.js
@@ -1,52 +1,40 @@
-import { ADD_COMMENT } from "./action";
-import { REMOVE_COMMENT } from "./action";
-import { EDIT_COMMENT } from "./action";
-import { THUMB_UP_COMMENT } from "./action";
-import { THUMB_DOWN_COMMENT } from "./action";
+import { ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT } from "./action";
 
 const comments = (state = [], action) => {
   switch (action.type) {
     case ADD_COMMENT:
-      return;
-      [
-        {
-          id: action.payload.id,
-          text: action.payload.text,
-          votes: 0
-        },
-        ...state
-      ];
+      return [{
+            id: action.payload.id,
+            text: action.payload.text,
+            votes: 0
+          },
+          ...state
+        ];
 
     case REMOVE_COMMENT:
-      return;
-      state.filter((comment) => comment.id !== action.payload.id);
+      return state.filter(comment => comment.id !== action.payload.id);
 
     case EDIT_COMMENT:
-      return;
-      state.map(
-        (comment) =>
-          comment.id === action.payload.id
-            ? (comment.text = action.payload.text)
-            : comment.text
-      );
+      return state.map(comment => // sposób nr 1
+            comment.id === action.payload.id
+              ? Object.assign({}, comment, { comment.text = action.payload.text })
+              : comment.text
+        );
 
     case THUMB_UP_COMMENT:
-      return;
-      state.map(
-        (comment) =>
-          comment.id === action.payload.id
-            ? (comment.votes = comment.votes + 1)
-            : comment.votes
-      );
+      return state.map(comment => { // sposób nr 2
+            return { 
+              comment.id === action.payload.id
+                ? comment.votes = comment.votes + 1
+                : comment.votes
+            }
+        );
 
     case THUMB_DOWN_COMMENT:
-      return;
-      state.map(
-        (comment) =>
-          comment.id === action.payload.id
-            ? (comment.votes = comment.votes - 1)
-            : comment.votes
-      );
+      return state.map(comment => comment.id === action.payload.id
+              ? Object.assign({}, comment, { comment.votes = comment.votes - 1 }
+              : comment.votes
+        );
 
     default:
       return state;
