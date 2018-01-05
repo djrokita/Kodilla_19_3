d3ac2df70f7ecb40218ce93843b7fbc006c5099f
Krzych <krzych.pierzchala@gmail.com>
2018-01-05 17:13:32 +0100

    Fixed reducer for comments

--- actions.js
+++ actions.js
@@ -1,54 +1,54 @@
-import uuid from uuid;
+import uuid from "uuid";
 
-export const ADD_COMMENT = 'ADD_COMMENT';
-export const EDIT_COMMENT = 'EDIT_COMMENT';
-export const REMOVE_COMMENT = 'REMOVE_COMMENT';
-export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+export const ADD_COMMENT = "ADD_COMMENT";
+export const EDIT_COMMENT = "EDIT_COMMENT";
+export const REMOVE_COMMENT = "REMOVE_COMMENT";
+export const THUMB_UP_COMMENT = "THUMB_UP_COMMENT";
+export const THUMB_DOWN_COMMENT = "THUMB_DOWN_COMMENT";
 
 const addComment = (text) => {
-	return {
-		type: ADD_COMMENT,
-		payload: {
-			text,
-			id: uuid.v4()
-		}
-	}
-}
+  return {
+    type: ADD_COMMENT,
+    payload: {
+      text,
+      id: uuid.v4()
+    }
+  };
+};
 
 const editComment = (text, id) => {
-	return {
-		type: EDIT_COMMENT,
-		payload: {
-			text,
-			id
-		}
-	}
-}
+  return {
+    type: EDIT_COMMENT,
+    payload: {
+      text,
+      id
+    }
+  };
+};
 
 const removeComment = (id) => {
-	return {
-		type: REMOVE_COMMENT,
-		payload: {
-			id
-		}
-	}
-}
+  return {
+    type: REMOVE_COMMENT,
+    payload: {
+      id
+    }
+  };
+};
 
 const getThumbUp = (id) => {
-	return {
-		type: THUMB_UP_COMMENT,
-		payload: {
-			id
-		}
-	}
-}
+  return {
+    type: THUMB_UP_COMMENT,
+    payload: {
+      id
+    }
+  };
+};
 
 const getThumbDown = (id) => {
-	return {
-		type: THUMB_DOWN_COMMENT,
-		payload: {
-			id
-		}
-	}
-}
+  return {
+    type: THUMB_DOWN_COMMENT,
+    payload: {
+      id
+    }
+  };
+};
--- comments.js
+++ comments.js
@@ -1,40 +1,47 @@
-import { ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT } from "./action";
+import {
+  ADD_COMMENT,
+  REMOVE_COMMENT,
+  EDIT_COMMENT,
+  THUMB_UP_COMMENT,
+  THUMB_DOWN_COMMENT
+} from "./actions";
 
 const comments = (state = [], action) => {
   switch (action.type) {
     case ADD_COMMENT:
-      return [{
-            id: action.payload.id,
-            text: action.payload.text,
-            votes: 0
-          },
-          ...state
-        ];
+      return [
+        {
+          id: action.payload.id,
+          text: action.payload.text,
+          votes: 0
+        },
+        ...state
+      ];
 
     case REMOVE_COMMENT:
       return state.filter(comment => comment.id !== action.payload.id);
 
     case EDIT_COMMENT:
-      return state.map(comment => // sposób nr 1
-            comment.id === action.payload.id
-              ? Object.assign({}, comment, { comment.text = action.payload.text })
-              : comment.text
-        );
+      return state.map(// sposób nr 1 )
+        comment  => comment.id === action.payload.id
+            ? Object.assign({}, comment, { text: action.payload.text })
+            : comment
+      );
 
     case THUMB_UP_COMMENT:
-      return state.map(comment => { // sposób nr 2
-            return { 
-              comment.id === action.payload.id
-                ? comment.votes = comment.votes + 1
-                : comment.votes
-            }
-        );
+      return state.map(comment => {
+        // sposób nr 2
+        return comment.id === action.payload.id
+          ? { ...comment, votes: comment.votes + 1 }
+          : comment;
+      });
 
     case THUMB_DOWN_COMMENT:
-      return state.map(comment => comment.id === action.payload.id
-              ? Object.assign({}, comment, { comment.votes = comment.votes - 1 }
-              : comment.votes
-        );
+      return state.map(
+        comment => comment.id === action.payload.id
+            ? Object.assign({}, comment, { votes: comment.votes - 1 })
+            : comment
+      );
 
     default:
       return state;
